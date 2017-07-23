a5a21490610789e1c743b6f356d10ce3d00e584b
dlmoraes <diegofrost96@gmail.com>
2017-07-22 22:06:18 -0300

    Ajustando content inicial

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -11,24 +11,6 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <nav id="navbar-center" className="navbar navbar-default navbar-fixed-top">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <a href="" className="navbar-brand">
-                                <img src="/images/tilst.png" className="img-responsive" alt="" />
-                            </a>
-                        </div>
-                        <div className="navbar-collapse collapse" id="navbar">
-                            <ul className="nav navbar-nav">
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
                 <h1>Kanban Board React.js Redux</h1>
             </div>
         )
